d35888e636377c0a97df7e1c39de75b8aecf9dcf
Jonathan63 <jalex4844@gmail.com>
2020-08-01 19:32:13 -0500

    "Controller container"

--- Backend/controllers/container.controller.js
+++ Backend/controllers/container.controller.js
@@ -31,4 +31,51 @@ containerCtrl.createContainer = async (req, res) => {
   });
 }
 
+containerCtrl.getContainer = async (req, res) => {
+  await Container.findById(req.params.id, (err, container) => {
+    if (err) {
+      console.log('Error:', err);
+      return;
+    }
+
+    res.json({
+      status: container
+    });
+  });
+}
+
+containerCtrl.editContainer = async (req, res) => {
+  const {id} = req.params;
+  const container = {
+    location: req.body.location,
+    lat: req.body.lat,
+    lng: req.body.lng
+  }
+  await Container.findByIdAndUpdate(id, {$set: container}, {new: true}, (err) => {
+    if (err) {
+      console.log('Error:', err);
+      return;
+    }
+
+    res.json({
+      status: 'Contenedor actualizado'
+    });
+  });
+}
+
+
+containerCtrl.deleteContainer = async (req, res) => {
+  console.log(req.params.id);
+  await Container.findByIdAndRemove(req.params.id, (err) => {
+    if (err) {
+      console.log('Error:', err);
+      return;
+    }
+
+    res.json({
+      status: 'Contenedor eliminado'
+    });
+  });
+}
+
 module.exports = containerCtrl;
