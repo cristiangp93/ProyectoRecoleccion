b415a7a3bd749362945f4e1a8b432a8d6baa7220
Jonathan63 <jalex4844@gmail.com>
2020-12-23 14:34:51 -0500

    "Modifique"

--- Backend/models/container.js
+++ Backend/models/container.js
@@ -3,7 +3,9 @@ const {Schema} = mongoose;
 
 const ContainerSchema = new Schema({
   location: {type: String, required: true},
+  cantidad: {type: Number, required: true},
   schedule_begin: {type: String, required: true},
+  schedule_end: {type: String, required: true},
   schedule_days_runs: {type: [String], required: true},
   lat: {type: Number, required: true},
   lng: {type: Number, required: true},
