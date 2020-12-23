9609c83c2fe11bce2722678b2d36f56d97831b48
Cristian <cpguillenp80@est.ucacue.edu.ec>
2020-12-22 23:10:23 -0500

    # 22-12-20 * Corrección de mapa en pagina principal

--- FrontEnd/src/app/layouts/auth-layout/home/home.component.ts
+++ FrontEnd/src/app/layouts/auth-layout/home/home.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {RutasService} from "../../../services/rutas.service";
-import {Sector} from "../../../models/sector";
-import {RouteSectorVehicleEmployeeService} from "../../../services/route-sector-vehicle-employee.service";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Route} from "../../../models/route";
+import {ContainerService} from "../../../services/container.service";
+import {Container} from "../../../models/container";
 
 @Component({
   selector: 'app-home',
@@ -17,27 +17,23 @@ export class HomeComponent implements OnInit {
   locationAccepted: boolean;
   /* Variables */
   public icon = {
-    url: 'http://earth.google.com/images/kml-icons/track-directional/track-4.png',
+    url: 'https://image.flaticon.com/icons/png/512/1686/1686033.png',
     scaledSize: {
-      width: 30,
-      height: 30
+      width: 40,
+      height: 40
     }
   }
-  sectorForm: FormGroup;
   selectedSector: any;
+  selectedContainer: any;
 
-  constructor(public _rSvES: RouteSectorVehicleEmployeeService,
-              public _rS: RutasService,
-              public fb: FormBuilder) {
+  constructor(public _rS: RutasService,
+              public _cS: ContainerService){
   }
 
   ngOnInit(): void {
-    this.getRouteSectorVehicleEmployee();
+    this.getRoutes();
+    this.getContainers();
     this.get();
-
-    this.sectorForm = this.fb.group({
-      sector: ['', Validators.required],
-    });
 
     navigator.permissions.query({ name: 'geolocation' })
       .then((resp) => {
@@ -48,20 +44,30 @@ export class HomeComponent implements OnInit {
 
 
 
-  async getRouteSectorVehicleEmployee() {
-    await this._rSvES.getRouteSectorVehicleEmployee().subscribe(resp => {
-      this._rSvES.routesSectorVehicleEmployee = resp as any[];
+  async getRoutes() {
+    await this._rS.getRoutes().subscribe(resp => {
+      this._rS.routes = resp as Route[];
+    }, error => {
+      console.log(`Error: ${error}`);
+    })
+  }
+
+  async getContainers() {
+    this.loading = true;
+    this._cS.getContainers().subscribe( resp =>{
+      this._cS.containers = resp as Container[];
     }, error => {
       console.log(`Error: ${error}`);
     });
   }
 
-  setSelected() {
-    this.selectedSector = this.sector;
+  setSelectedSector(sector ) {
+    this.selectedSector = sector;
   }
 
-  get sector() {
-    return this.sectorForm.get('sector').value;
+  setSelectedContainer( container ) {
+    this.selectedContainer = container;
+    console.log(this.selectedContainer)
   }
 
   get(){
